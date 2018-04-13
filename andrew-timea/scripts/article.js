3c04be8affd45aa13ab0db7fe35b886920fa9a0c
Andrew Baik <andrewbaik@outlook.com>
2018-04-13 09:56:11 -0700

    switch driver

--- andrew-timea/scripts/article.js
+++ andrew-timea/scripts/article.js
@@ -3,23 +3,28 @@
 let articles = [];
 
 function Article (rawDataObj) {
-  this.author = rawDataObj.author;
-  this.authorUrl = rawDataObj.authorUrl;
-  this.title = rawDataObj.title;
-  this.category = rawDataObj.category;
-  this.body = rawDataObj.body;
-  this.publishedOn = rawDataObj.publishedOn;
+  // this.author = rawDataObj.author;
+  // this.authorUrl = rawDataObj.authorUrl;
+  // this.title = rawDataObj.title;
+  // this.category = rawDataObj.category;
+  // this.body = rawDataObj.body;
+  // this.publishedOn = rawDataObj.publishedOn;
+
+  for(let key in rawDataObj) {
+    this[key] = rawDataObj[key];
+  }
 }
 
 Article.prototype.toHtml = function() {
   // TODO: Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars.
+  let templates = $('#articles-temp').html();
+  let renderTemp = Handlebars.compile(templates);
 
   // REVIEW: If your template will use properties that aren't on the object yet, add them.
   // Since your template can't hold any JS logic, we need to execute the logic here.
   // The result is added to the object as a new property, which can then be referenced by key in the template.
   // For example, you might want to display how old a post is, or say "(draft)" if it has no publication date:
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
-
 
   this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
 
@@ -31,11 +36,13 @@ Article.prototype.toHtml = function() {
   // }
 
   // TODO: Use the method that Handlebars gave you to return your filled-in html template for THIS article.
-
+  return renderTemp(this);
 };
 
 // COMMENT: Why are there parentheses around "(a,b)" in the .sort() method, but not around the "articleObject" or "article" arguments in the .forEach() methods?
 // PUT YOUR RESPONSE HERE
+// (a,b) is a part of an arrow function inside the method and it contains multiple placeholders.
+
 rawData.sort((a,b) => {
   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
 });
@@ -45,5 +52,5 @@ rawData.forEach(articleObject => {
 });
 
 articles.forEach(article => {
-  $('#articles').append(article.toHtml());
+  $('#articlesHere').append(article.toHtml());
 });
